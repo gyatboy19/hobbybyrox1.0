071776eb380857179c702ce6d4b088a619ec1dac
enthusiast <cloistermanwilliem@gmail.com>
2025-09-06 22:24:56 +0200

    Update index.js
    enable CORS for upload API

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ import express from "express";
 import uploadRoute from "./upload-to-github.js";
 
 const app = express();
+// Allow cross-origin requests (so GitHub Pages can call Render)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 app.use(uploadRoute);
 
 const PORT = process.env.PORT || 3000;
